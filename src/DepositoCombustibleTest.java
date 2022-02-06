34d7b79cc3ada9e6f9a954859c976831aff5815f
Jose Antonio Moreno <joseantoniomoreno74@hotmail.com>
2022-02-06 23:20:22 +0100

    vension final

--- src/DepositoCombustibleTest.java
+++ src/DepositoCombustibleTest.java
@@ -36,12 +36,16 @@ public class DepositoCombustibleTest {
 
 	@Test
 	public void testFill() {
-		fail("No implementado aun");
+		DepositoCombustible tank = new DepositoCombustible(40.0,20.0);
+		tank.fill(10);
+		assertEquals(tank.getDepositoNivel(),30,0);
 	}
 
 	@Test
 	public void testConsumir() {
-		fail("No implementado aun");
+		DepositoCombustible tank = new DepositoCombustible(40.0,20.0);
+		tank.consumir(10);
+		assertEquals(tank.getDepositoNivel(),10,0);
 	}
 
 }
